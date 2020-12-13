96ed277df48fa900dfa5701b43e40120394e7acd
Alex Vakula <albanec777@meta.ua>
2020-12-13 14:53:16 +0100

    add Zero for one sign time  '0${num}'

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -49,36 +49,48 @@ window.addEventListener("DOMContentLoaded", () => {
       minutes = Math.floor((t / 1000 / 60) % 60),
       seconds = Math.floor((t / 1000) % 60);
 
-      return {
-        'total': t,
-        'days': days,
-        'hours': hours,
-        'minutes': minutes,
-        'seconds': seconds
-      };
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
   }
 
   function setClock(selector, endTime) {
     const timer = document.querySelector(selector),
-          days = timer.querySelector('#days'),
-          hours = timer.querySelector('#hours'),
-          minutes = timer.querySelector('#minutes'),
-          seconds = timer.querySelector('#seconds'),
-          timeInterval = setInterval(updateClock, 1000);
+      days = timer.querySelector("#days"),
+      hours = timer.querySelector("#hours"),
+      minutes = timer.querySelector("#minutes"),
+      seconds = timer.querySelector("#seconds"),
+      timeInterval = setInterval(updateClock, 1000);
 
-        function updateClock() {
-          const t = getTimeRemaining(endTime);
+    updateClock();
 
-          days.innerHTML = t.days;
-          hours.innerHTML = t.hours;
-          minutes.innerHTML = t.minutes;
-          seconds.innerHTML = t.seconds;
+    function updateClock() {
+      const t = getTimeRemaining(endTime);
 
-          if (t.total <= 0) {
-            clearInterval(timeInterval);
-          }
-        }
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
   }
 
-  setClock('.timer', deadLine);
+  setClock(".timer", deadLine);
+
+  //end Timer
 });
