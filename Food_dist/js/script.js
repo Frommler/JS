7c58996f77fb73d2f0184a09bf24458785a7b7da
Alex Vakula <albanec777@meta.ua>
2020-12-20 16:24:13 +0100

    add style.overflow

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -95,8 +95,20 @@ window.addEventListener("DOMContentLoaded", () => {
   //end Timer
 
   //Modal
-  const modalTrigger = document.querySelectorAll('[ data-modal]'),
-        modal = document.querySelector('.modal'),
-        modalCloseBtn = document.querySelector('[data-close]');
+  const modalTrigger = document.querySelector("[ data-modal]"),
+    modal = document.querySelector(".modal"),
+    modalCloseBtn = document.querySelector("[data-close]");
+
+  modalTrigger.addEventListener("click", () => {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+    document.body.style.overflow = "hidden";
+  });
+
+  modalCloseBtn.addEventListener("click", () => {
+    modal.classList.add("hide");
+    modal.classList.remove("show");
+    document.body.style.overflow = "";
+  });
   //end Modal
 });
